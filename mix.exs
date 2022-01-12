2bc808307a31173c15483c8cb7167d60a827ff38
David Pavlík <david@dpavlik.cz>
2022-01-12 16:18:33 +0100

    Bump version to 0.3.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DockerCompose.MixProject do
   def project do
     [
       app: :docker_compose,
-      version: "0.3.0",
+      version: "0.3.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
